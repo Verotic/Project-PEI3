0cf0828f14d4d6cc1c4843d4211ca8e5cf2ab76a
julia <2024114388@uac.pt>
2026-04-13 14:35:16 +0000

    Update main.js

--- js/main.js
+++ js/main.js
@@ -4,33 +4,38 @@ import { TiltEffect } from './modules/TiltEffect.js';
 import { ContactForm } from './modules/ContactForm.js';
 import { AnimationManager } from './modules/Animations.js';
 import { ChartManager } from './modules/ChartManager.js';
+import { Database } from './core/Database.js';
 
 /**
  * Main application file.
  * Responsible for initializing all modules.
  */
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', async () => {
   console.log('Application started successfully (Modularized).');
 
-  // 1. Initialize Scroll to Top Button (Adriano)
+  // 1. Inicializa a Base de Dados (Julia)
+  const db = new Database('CACADatabase', 1);
+  await db.connect(); // Método que cria as stores de eventos e newsletter
+
+  // 2. Initialize Scroll to Top Button (Adriano)
   new ScrollManager('scrollTopBtn');
 
-  // 2. Initialize 3D Tilt Effect on Cards (Adriano)
+  // 3. Initialize 3D Tilt Effect on Cards (Adriano)
   new TiltEffect('.area-card');
 
-  // 3. Initialize Hero Carousel (Adriano)
+  // 4. Initialize Hero Carousel (Adriano)
   new HeroCarousel('.hero-carousel', [
     'assets/images/caca-1',
     'assets/images/caca-2'
   ]);
 
-  // 4. Initialize Contact Form (Iulia)
+  // 5. Initialize Contact Form (Iulia)
   new ContactForm('#contactForm');
 
-  // 5. Initialize Animations (David)
+  // 6. Initialize Animations (David)
   // Small delay to ensure DOM is ready and layout stabilized
   setTimeout(() => {
     new AnimationManager();
     new ChartManager('investmentChart'); // Initialize Chart with optimization
   }, 100);
-});
+})
